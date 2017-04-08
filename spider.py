ecdf74e33e4f9f3c67e315b438e942995cbb565b
TheDen <deniskhoshaba@gmail.com>
2017-04-08 20:27:13 +1000

    improve latlong finding

--- spider.py
+++ spider.py
@@ -4,6 +4,9 @@ import scrapy
 import re
 from urlparse import urlparse
 import json
+import urllib2
+import urllib
+import StringIO
 
 from geopy.geocoders import Nominatim
 geolocator = Nominatim()
@@ -34,15 +37,29 @@ class DineSpider(scrapy.Spider):
        address = response.xpath("//table//td/text()").extract()[3]
        suburb = response.xpath("//table//td/text()").extract()[4]
        date = response.xpath("//table//td/text()").extract()[6]
-       location = geolocator.geocode(address)
-       if location:
-        latlong = location.latitude,location.longitude
-       else:
-        latlong = "not found"
+       latlong = decodeAddressToCoordinates(address+suburb)
+       if latlong is None:
+         latlong = "Not Found"
        yield {
                 'name': name,
                 'address': address+suburb,
                 'date': date, 
                 'url': response.url,
                 'latlong': latlong
-        } 
+        }
+
+def decodeAddressToCoordinates( address ):
+        urlParams = {
+                'address': address,
+                'sensor': 'false',
+        }
+        url = 'http://maps.google.com/maps/api/geocode/json?' + urllib.urlencode( urlParams )
+        response = urllib2.urlopen( url )
+        responseBody = response.read()
+
+        body = StringIO.StringIO( responseBody )
+        result = json.load( body )
+        if 'status' not in result or result['status'] != 'OK':
+                return None
+        else:
+                return result['results'][0]['geometry']['location']['lat'], result['results'][0]['geometry']['location']['lng'] 
